edc942da74b05e906daea64b4df639d39a8ac1c2
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-02-05 22:16:00 +0100

    more specific error messages from interceptors

--- appHttpInterceptors.js
+++ appHttpInterceptors.js
@@ -12,14 +12,14 @@ angular.module('coAuth')
             },
             responseError: function (response) {
                 if (response.config.params && !response.config.params.muteNotification) {
-                    $injector.get('$rootScope').$broadcast('toast', { type: 'error', message: response });
+                    $injector.get('$rootScope').$broadcast('toast', { type: 'httpResponseError', message: response });
                 }
                 return $q.reject(response);
             },
             response: function (config) {
                 if ((config.config.method === 'POST' || config.config.method === 'PUT') && config.status === 200) {
                     if (config.config.url.indexOf('/Users/login') === -1) {
-                        $injector.get('$rootScope').$broadcast('toast', { type: 'success', message: config });
+                        $injector.get('$rootScope').$broadcast('toast', { type: 'httpPostSuccess', message: config });
                     }
                 }
                 return config;
